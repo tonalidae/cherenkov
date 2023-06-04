c4481769eb3117f4c166ccc09562b13839fedd2c
Stephanie Cely <eccelyr@unal.edu.co>
2023-06-04 14:32:49 -0500

    feat: read all data files in dir, extract E

---
+++ data_prep.py
@@ -0,0 +1,23 @@
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import csv
+import scipy as sp
+
+def read_data(file_path):
+
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        data = []
+        for column in reader:
+            column[0] = column[0].replace('(', '').replace("'" , '')
+            column[2] = column[2].replace(')', '').replace("'" , '')
+            column[1] = column[1].replace("'" , '')
+            column[0] = float(column[0])
+            column[1] = float(column[1])
+            column[2] = float(column[2])
+            data.append(column)
+        data = np.array(data)
+        print(data.shape)
+    return data
+
